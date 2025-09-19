8cd88c90d26c15a766af104a7415d5299561bb75
nagasai <nagasaikonderu@gmail.com>
2025-09-19 15:17:58 +0530

    script

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
-set -e
+apt-get update
+apt-get install -y unzip curl
 
-# Update system
-apt-get update -y
+apt-get install -y openjdk-17-jdk gnupg git
 
-
-# Install Java 21 (OpenJDK)
-apt-get install -y openjdk-21-jdk
-
-# Install Terraform
 TERRAFORM_VERSION="1.12.1"
 curl -fsSL https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -o terraform.zip
 unzip terraform.zip
-chmod +x terraform
 mv terraform /usr/local/bin/
 rm terraform.zip
 
-echo "Java version:"
-java -version
-
 echo "Terraform installed:"
 terraform -version
-
-# Create Jenkins directory
-mkdir -p /home/ubuntu/jenkins
-chown ubuntu:ubuntu /home/ubuntu/jenkins
-
-echo "Jenkins slave node ready with Java 21 and Terraform"
